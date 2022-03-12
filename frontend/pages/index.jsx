876d40541a4f15a08d7fe0142635f6b451789ae4
Matt Mannion <mattpmannion@gmail.com>
2022-03-12 18:17:38 -0500

    more documentation and refactoring

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -53,14 +53,14 @@ export default function Home() {
             alt="mother and baby holding hands"
           /> */}
 
-      {/* <footer className={styles.footer}>
+      {/* <footer className="home__footer">
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <span className={styles.logo}>
+          <span className="home__logo">
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
